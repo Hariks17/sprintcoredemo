654a8c88da8d2e077bb042ae43c08bfa40792084
Hariks17 <hariksh.94@gmail.com>
2024-06-20 13:43:56 +0530

    Added Starter

---
+++ src/test/java/com/hari/java/sprintcoredemo/SprintcoredemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hari.java.sprintcoredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintcoredemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
